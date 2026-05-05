f2830e3179848677653f2ae6bf69c14d89b1f93c
ritoaguila-crypto <rito.aguila@alumnos.udg.mx>
2026-05-05 09:21:00 -0600

    conexion varibles de entorno

--- app/db/conexion.js
+++ app/db/conexion.js
@@ -1,17 +1,16 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-    host: "bgmdntp0fxpecf7ftkxe-mysql.services.clever-cloud.com",
-    user: "ulgznnricuicq9eo",
-    password: "5l9h6AuI82mbKStL0LCm",
-    database: "bgmdntp0fxpecf7ftkxe",
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 
-    
     ssl: {
         rejectUnauthorized: false
     }
